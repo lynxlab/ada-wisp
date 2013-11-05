0e9d1cf7a8c6465f2d03bff8c923b022f6a3c66a
graffio <graffio@lynxlab.com>
2013-11-05 13:38:47 +0100

    fixed bug

--- js/main/index.js
+++ js/main/index.js
@@ -1,7 +1,8 @@
 function initDoc() {
      $j(function() {
         $j( ".column" ).sortable({
-            connectWith: ".column"
+            connectWith: ".column",
+//            handle: ".porlet-header"
         });
         $j( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
             .find( ".portlet-header" )
@@ -13,7 +14,7 @@ function initDoc() {
             $j( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
             $j( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
         });
-        $j( ".column" ).disableSelection();
+//        $j( ".column" ).disableSelection();
     });    
     
     /*
