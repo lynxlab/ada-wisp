adcb3ab228e4d99ca125e9751a58e293933d68e7
Giorgio Consorti <g.consorti@lynxlab.com>
2014-12-19 15:01:38 +0100

    added defines for HOLIS menu generation

--- include/menu_class.inc.php
+++ include/menu_class.inc.php
@@ -110,8 +110,38 @@ class Menu
      			$_SESSION['sess_userObj']->getSerialNumber()==AMA_TYPE_USER_MAGISTRATE &&
      			!defined('IS_MAGISTRATE')) {
      				define ('IS_MAGISTRATE', true);
-     	}     	
-     	if (!defined ('IS_MAGISTRATE')) define ('NOT_MAGISTRATE',true);
+     	} else if (isset($_SESSION['sess_userObj']) && $_SESSION['sess_userObj'] instanceof ADALoggableUser &&
+     			$_SESSION['sess_userObj']->getType()==AMA_TYPE_STUDENT &&
+     			$_SESSION['sess_userObj']->getSerialNumber()==AMA_TYPE_USER_LAWYER &&
+     			!defined('IS_LAWYER')) {
+     				define ('IS_LAWYER', true);
+     	} else if (isset($_SESSION['sess_userObj']) && $_SESSION['sess_userObj'] instanceof ADALoggableUser &&
+     			$_SESSION['sess_userObj']->getType()==AMA_TYPE_STUDENT &&
+     			$_SESSION['sess_userObj']->getSerialNumber()==AMA_TYPE_USER_GENERIC &&
+     			!defined('IS_GENERIC')) {
+     				define ('IS_GENERIC', true);
+     	} else if (isset($_SESSION['sess_userObj']) && $_SESSION['sess_userObj'] instanceof ADALoggableUser &&
+     			$_SESSION['sess_userObj']->getType()==AMA_TYPE_STUDENT &&
+     			$_SESSION['sess_userObj']->getSerialNumber()==AMA_TYPE_USER_AUX &&
+     			!defined('IS_AUX')) {
+     				define ('IS_AUX', true);
+     	}  else if (isset($_SESSION['sess_userObj']) && $_SESSION['sess_userObj'] instanceof ADALoggableUser &&
+     			$_SESSION['sess_userObj']->getType()==AMA_TYPE_TUTOR &&
+     			!defined('IS_TUTOR')) {
+     				define ('IS_TUTOR', true);
+     	}  else if (isset($_SESSION['sess_userObj']) && $_SESSION['sess_userObj'] instanceof ADALoggableUser &&
+     			$_SESSION['sess_userObj']->getType()==AMA_TYPE_AUTHOR &&
+     			!defined('IS_AUTHOR')) {
+     				define ('IS_AUTHOR', true);
+     	}
+     	
+     	if (!defined('IS_MAGISTRATE')) define('NOT_MAGISTRATE', true);
+     	if (!defined('IS_LAWYER'))     define('NOT_LAWYER', true);
+     	if (!defined('IS_GENERIC'))    define('NOT_GENERIC', true);
+     	if (!defined('IS_AUX'))        define('NOT_AUX', true);
+     	if (!defined('IS_TUTOR'))      define('NOT_TUTOR', true);
+     	if (!defined('IS_AUTHOR'))     define('NOT_AUTHOR', true);
+     	
      	/**
      	 * end of defines used in holis version only
      	 */
