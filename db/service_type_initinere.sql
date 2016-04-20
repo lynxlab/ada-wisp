ad353d7a5d89b0e61b93f022de1c3c0d6bd28318
Giorgio Consorti <g.consorti@lynxlab.com>
2016-04-20 13:35:37 +0200

    fix to show 'in itenere' service level to the student

--- db/service_type_initinere.sql
+++ db/service_type_initinere.sql
@@ -1,2 +1,2 @@
 INSERT INTO `service_type` (`livello_servizio`, `nome_servizio`, `descrizione_servizio`, `custom_1`, `custom_2`, `custom_3`, `hiddenFromInfo`, `isPublic`) VALUES
-(98, 'Colloquio in itinere', NULL, NULL, NULL, NULL, 1, 0);
+(98, 'Colloquio in itinere', NULL, NULL, NULL, NULL, 0, 0);
